b9272c3f56799ce029ff445c29bc46d586a9aee7
Yuyi0011 <yz@smartbuyers.dk>
2023-08-31 15:14:10 +0200

    Hej

---
+++ src/main/java/dk/kea/simplespringymal/SimpleSpringYmalApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.simplespringymal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringYmalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringYmalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/simplespringymal/SimpleSpringYmalApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.simplespringymal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringYmalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
